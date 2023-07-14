b9ed6b510336e35a133fef02054f2d2438f2429e
f.afshani <fatemeh.afshani78@gmail.com>
2023-07-14 12:04:49 +0330

    feat: add redis as game database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,22 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { GameModule } from './game/game.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
 
-const { uri } = config.get('mongo');
+const { uri: mongoUri } = config.get('mongo');
+const { url: redisUrl } = config.get('redis');
+console.log('#### redis url', redisUrl);
 
 @Module({
-  imports: [MongooseModule.forRoot(uri), UserModule, GameModule],
+  imports: [
+    MongooseModule.forRoot(mongoUri),
+    RedisModule.forRoot({
+      config: {
+        url: redisUrl,
+      },
+    }),
+    UserModule,
+    GameModule,
+  ],
 })
 export class AppModule {}
